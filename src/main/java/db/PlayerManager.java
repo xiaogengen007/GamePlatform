94f058875975f28d2634cf99714cc8898c3ac06f
itachi4869 <kaitokidli@qq.com>
2017-11-30 01:11:13 +0800

    Add recordPlayer class and checkLogin class to PlayerManager. (#3)

--- src/main/java/db/PlayerManager.java
+++ src/main/java/db/PlayerManager.java
@@ -1,6 +1,46 @@
-public	class	PlayerManager
-{
-	public	static	void	setup()
-	{
+package db;
+
+import java.sql.*;
+
+public class PlayerManager {
+	public static void recordPlayer(String plyName, String plyKey) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			//System.out.println("Open database successfully");
+			
+			stmt = c.createStatement();
+			String sql = "INSERT INTO player(p_id, p_name, p_key, p_point) " +
+						 "VALUES (null, '" + plyName + "', '" + plyKey + "', null);";
+			stmt.executeUpdate(sql);
+			
+			stmt.close();
+			c.commit();
+			c.close();
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ":" + e.getMessage());
+			System.exit(0);
+		}
+		//System.out.println("Records create successfully");
+	}
+	
+	public static boolean checkLogin(String plyName, String inputKey) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			//System.out.println("Open database successfully");
+			
+			stmt = c.createStatement();
+			String sql = "SELECT p_key FROM player" + 
+						 "WHERE p_name = '" + plyName + "';";
+			ResultSet rs = stmt.executeQuery(sql);
+		}
 	}
 }
