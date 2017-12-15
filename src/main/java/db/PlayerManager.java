42f4f2b66e1385cc1796adebae341b96122837e3
itachi4869 <kaitokidli@qq.com>
2017-12-15 20:51:48 +0800

    Finish modifyPoint module in PlayerManager class. (#3)

--- src/main/java/db/PlayerManager.java
+++ src/main/java/db/PlayerManager.java
@@ -8,8 +8,10 @@ public class PlayerManager {
 		Statement stmt = null; //SQL语句环境
 		try {
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:D:/resource/datebase/Gameplatform.db");
-			c.setAutoCommit(false);		
+			c = DriverManager.getConnection("jdbc:sqlite:Gameplatform.db");
+			c.setAutoCommit(false);
+			System.out.println("Open database successfully");
+			
 			stmt = c.createStatement();
 			String sql_check = "SELECT * FROM player WHERE p_name = '"
 							 + plyName + "';"; //在数据库中寻找该用户名
@@ -18,7 +20,6 @@ public class PlayerManager {
 				stmt.close();
 				c.commit();
 				c.close();
-				System.out.println("错误类型1：该用户名已经存在");
 				return 1; //错误类型1：该用户名已经存在
 			}
 			else {
@@ -28,58 +29,17 @@ public class PlayerManager {
 				stmt.close();
 				c.commit();
 				c.close();
-				System.out.println("成功添加该用户");
+			
+				System.out.println("Records create successfully");
 				return 0; //成功添加该用户，返回0
 			}	
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ":" + e.getMessage());
-			System.out.println("错误类型2：数据库异常，注册失败");
 			return 2; //错误类型2：数据库异常，注册失败
 		}
 	}
 	
 	public static int checkLogin(String plyName, String inputKey) { //检查登录
-		Connection c = null;
-		Statement stmt = null;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:D:/resource/datebase/Gameplatform.db");
-			c.setAutoCommit(false);		
-			stmt = c.createStatement();
-			String sql = "SELECT * FROM player WHERE p_name = '"
-					 + plyName + "';"; //在数据库中寻找该用户名
-			ResultSet rs = stmt.executeQuery(sql);
-			if(rs.next()) {//存在该用户
-				if(inputKey.equals(rs.getString("p_key"))) { //输入密码与用户名密码匹配
-					stmt.close();
-					c.commit();
-					c.close();
-					System.out.println("登录成功");
-					return 0; //登录成功，返回0
-				}
-				else {
-					stmt.close();
-					c.commit();
-					c.close();
-					System.out.println("错误类型1：用户名与密码不匹配");
-					return 1; //错误类型1：用户名与密码不匹配
-				}
-			}
-			else {
-				stmt.close();
-				c.commit();
-				c.close();
-				System.out.println("错误类型2：该用户不存在（未注册）");
-				return 2; //错误类型2：该用户不存在（未注册）
-			}
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ":" + e.getMessage());
-			System.out.println("错误类型3：数据库异常，登录失败");
-			return 3;//错误类型3：数据库异常，登录失败
-		}
-	}
-	
-	public static int getPoint(String plyName) {
 		Connection c = null;
 		Statement stmt = null;
 		try {
@@ -90,14 +50,55 @@ public class PlayerManager {
 			
 			stmt = c.createStatement();
 			String sql = "SELECT * FROM player WHERE p_name = '"
-					+ plyName + "';"; //在数据库中寻找该用户名
+					 + plyName + "';"; //在数据库中寻找该用户名
 			ResultSet rs = stmt.executeQuery(sql);
-			if(rs.next()) { //该用户存在
-				int rtpoint = rs.getInt("p_point"); //存储该用户积分值
+			if(rs.next()) {//存在该用户
+				if(inputKey.equals(rs.getString("p_key"))) { //输入密码与用户名密码匹配
+					//System.out.println("Login successfully");
+					stmt.close();
+					c.commit();
+					c.close();
+					return 0; //登录成功，返回0
+				}
+				else {
+					//System.out.println(rs.getString("p_key"));
+					//System.out.println(inputKey);
+					stmt.close();
+					c.commit();
+					c.close();
+					return 1; //错误类型1：用户名与密码不匹配
+				}
+			}
+			else {
 				stmt.close();
 				c.commit();
 				c.close();
-				return rtpoint; //返回该用户积分值
+				return 2; //错误类型2：该用户不存在（未注册）
+			}
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ":" + e.getMessage());
+			return 3;//错误类型3：数据库异常，登录失败
+		}
+	}
+
+	public static int getPoint(String plyName) { //获取用户积分
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:Gameplatform.db");
+			c.setAutoCommit(false);
+			
+			stmt = c.createStatement();
+			String sql = "SELECT * FROM player WHERE p_name = '"
+					+ plyName + "';"; //在数据库中寻找该用户名
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()) { //该用户存在
+				int rtPoint = rs.getInt("p_point"); //存储该用户积分值
+				stmt.close();
+				c.commit();
+				c.close();
+				return rtPoint; //返回该用户积分值
 			}
 			else { //该用户不存在
 				stmt.close();
@@ -105,10 +106,39 @@ public class PlayerManager {
 				c.close();
 				return Integer.MIN_VALUE; //用户不存在
 			}
-			
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ":" + e.getMessage());
 			return Integer.MIN_VALUE; //查询积分异常
+		}
+	}
+	
+	public static boolean modifyPoint(String plyName, int deltaPoint) { //修改用户积分
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:Gameplatform.db");
+			c.setAutoCommit(false);
+			
+			stmt = c.createStatement();
+			int prePoint = testPlayerManager.getPoint(plyName);// 获得原分数
+			if(prePoint == Integer.MIN_VALUE) {
+				return false; //获取分数异常
+			}
+			String newPoint = String.valueOf(prePoint + deltaPoint); //得到现在的分数(字符形式)
+			
+			String sql = "UPDATE player SET p_point = " + newPoint + " WHERE p_name = '"
+					+ plyName + "';";
+			stmt.executeUpdate(sql); //更新用户的积分
+			
+			stmt.close();
+			c.commit();
+			c.close();
+			return true; //更新积分成功
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ":" + e.getMessage());
+			return false; //修改用户积分异常
 		}
 	}
 }
