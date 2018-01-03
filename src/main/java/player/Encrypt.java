a9fe180d83d307f6acfdef55955d45c477eb8141
chuzhumin98 <chuzhumin98@gmail.com>
2018-01-03 17:00:36 +0800

    update Encrypt 3

--- src/main/java/player/Encrypt.java
+++ src/main/java/player/Encrypt.java
@@ -42,16 +42,16 @@ public class Encrypt {
 		if (strText != null && strText.length() > 0) {  
 			try {  
 				// SHA 加密开始  
-				// 创建加密对象 并傳入加密類型  
+				// 创建加密对象 并传入加密类型
 				MessageDigest messageDigest = MessageDigest.getInstance(strType);  
 				// 传入要加密的字符串  
 				messageDigest.update(strText.getBytes());  
-				// 得到 byte 類型结果  
+				// 得到 byte 类型结果  
 				byte byteBuffer[] = messageDigest.digest();  
 				
-				// 將 byte 轉換爲 string  
+				// 将 byte 转换为 string  
 				StringBuffer strHexString = new StringBuffer();  
-				// 遍歷 byte buffer  
+				// 遍历 byte buffer  
 				for (int i = 0; i < byteBuffer.length; i++) {  
 					String hex = Integer.toHexString(0xff & byteBuffer[i]);  
 					if (hex.length() == 1) {  
@@ -59,7 +59,7 @@ public class Encrypt {
 					}  
 					strHexString.append(hex);  
 				}  
-				// 得到返回結果  
+				// 得到返回结果  
 				strResult = strHexString.toString();  
 			}  
 			catch (NoSuchAlgorithmException e) {  
@@ -70,7 +70,7 @@ public class Encrypt {
 		return strResult;  
 	}  
   
-	/*
+	/**
 	 * 传进来一个password,进行加盐哈希加密处理，返回一个数组
 	 * 第一个为SHA256的密码，第二个为盐值
 	 */
